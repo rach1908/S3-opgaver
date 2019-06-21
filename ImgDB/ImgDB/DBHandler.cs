876c6e8022796e897300d881a5b9356a04cdc704
Rasmus Christophersen <r.t.christophersen@gmail.com>
2019-06-21 12:00:29 +0200

    new project, db and ui and classes up and running, dbhandler and file selction next

---
+++ ImgDB/ImgDB/DBHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgDB
+{
+    class DBHandler
+    {
+        const string conString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ImgDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+    }
+}
---
+++ ImgDB/ImgDB/Image.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgDB
+{
+    class Image
+    {
+        public Image(string title, string serializedImage)
+        {
+            Title = title;
+            SerializedImage = serializedImage;
+        }
+
+        public Image(int iD, string title, string serializedImage)
+        {
+            ID = iD;
+            Title = title;
+            SerializedImage = serializedImage;
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string SerializedImage { get; set; }
+    }
+}
